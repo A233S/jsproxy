abe0adb06a63fb85e3b6517ef8904a6dee43fefb
OASEN <71011762+A233S@users.noreply.github.com>
2023-10-03 08:46:28 +0800

    Update index.js

--- cf-worker/index.js
+++ cf-worker/index.js
@@ -15,6 +15,7 @@ const PREFLIGHT_INIT = {
     'access-control-allow-origin': '*',
     'access-control-allow-methods': 'GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS',
     'access-control-max-age': '1728000',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
   }),
 }
 
